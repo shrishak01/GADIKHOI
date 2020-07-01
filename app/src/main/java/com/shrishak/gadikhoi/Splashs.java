c99ebdea9e5ea78057f6b853bcb20bae49a845ec
shrishak shrestha <linkedshrishak@gmail.com>
2020-07-01 19:13:14 +0545

    Splash java written with timer 3 seconds

--- app/src/main/java/com/shrishak/gadikhoi/Splashs.java
+++ app/src/main/java/com/shrishak/gadikhoi/Splashs.java
@@ -2,7 +2,9 @@ package com.shrishak.gadikhoi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashs extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class Splashs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashs);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent openHome = new Intent(Splashs.this, MainActivity.class);
+                startActivity(openHome);
+                finish();
+            }
+        }, 3000);
+
     }
 }
