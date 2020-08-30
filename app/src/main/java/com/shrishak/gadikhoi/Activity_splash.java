70d02a6daa5c61896d63a6b9def27d379c74dea1
shrishak01 <linkedshrishak@gmail.com>
2020-08-30 19:30:40 +0545

    splash screen error in android 10

--- app/src/main/java/com/shrishak/gadikhoi/Activity_splash.java
+++ app/src/main/java/com/shrishak/gadikhoi/Activity_splash.java
@@ -6,19 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class Splashs extends AppCompatActivity {
+public class Activity_splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashs);
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent openHome = new Intent(Splashs.this, MainActivity.class);
-                startActivity(openHome);
+                Intent i = new Intent(Activity_splash.this, MainActivity.class);
+                startActivity(i);
                 finish();
             }
         }, 3000);
