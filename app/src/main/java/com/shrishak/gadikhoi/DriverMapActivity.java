ed6121b847a1d8ddc758a91a42d11089f00e0b76
shrishak01 <linkedshrishak@gmail.com>
2020-09-03 17:05:19 +0545

    driver map activity fixed

--- app/src/main/java/com/shrishak/gadikhoi/DriverMapActivity.java
+++ app/src/main/java/com/shrishak/gadikhoi/DriverMapActivity.java
@@ -68,16 +68,14 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
     }
     private void getAssignedCustomer(){
         String driverId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference assignedCustomerRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(driverId);
+        DatabaseReference assignedCustomerRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(driverId).child("customerRideId");
         assignedCustomerRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if(dataSnapshot.exists()){
-                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
-                    if(map.get("customerRideId")!=null){
-                        customerId = map.get("customerRideId").toString();
+                        customerId = dataSnapshot.getValue().toString();
                         getAssignedCustomerPickupLocation();
-                    }
+
                 }
             }
 
@@ -152,9 +150,11 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
                 case "":
                     geoFireWorking.removeLocation(userId);
                     geoFireAvailable.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
+
                     break;
                 default:
-
+                    geoFireAvailable.removeLocation(userId);
+                    geoFireWorking.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
                     break;
             }
 
