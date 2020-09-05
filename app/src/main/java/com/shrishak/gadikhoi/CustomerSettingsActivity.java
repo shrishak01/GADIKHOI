1d52bef44726853c6251aae902560cae949680bf
shrishak01 <linkedshrishak@gmail.com>
2020-09-05 18:09:06 +0545

    data pulled from database but cannot push

--- app/src/main/java/com/shrishak/gadikhoi/CustomerSettingsActivity.java
+++ app/src/main/java/com/shrishak/gadikhoi/CustomerSettingsActivity.java
@@ -3,12 +3,38 @@ package com.shrishak.gadikhoi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CustomerSettingsActivity extends AppCompatActivity {
+    private EditText mNameField, mPhoneField, mLocation, mOccupation;
+    private Button mBack, mConfirm;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mCustomerDatabase;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_settings);
+        mNameField = (EditText) findViewById(R.id.name);
+        mPhoneField = (EditText) findViewById(R.id.phone);
+        mLocation = (EditText) findViewById(R.id.location);
+        mOccupation = (EditText) findViewById(R.id.occupation);
+
+        mBack = (Button) findViewById(R.id.confirm);
+        mConfirm = (Button) findViewById(R.id.back);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child("userID");
+
+
+
+
     }
 }
