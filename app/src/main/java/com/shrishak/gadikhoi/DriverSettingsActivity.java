83e1354314a4440a02d9bc3a6f2942070c2c63d0
shrishak01 <linkedshrishak@gmail.com>
2020-09-09 17:26:50 +0545

    driver profile is in progress

--- app/src/main/java/com/shrishak/gadikhoi/DriverSettingsActivity.java
+++ app/src/main/java/com/shrishak/gadikhoi/DriverSettingsActivity.java
@@ -1,23 +1,216 @@
 package com.shrishak.gadikhoi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class DriverSettingsActivity extends AppCompatActivity {
+    private EditText mNameField, mPhoneField, mLicense, mVehicle, mType;
+
+    private Button mBack, mConfirm;
+
+    private ImageView mProfileImage;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDriverDatabase;
+
+    private String userID;
+
+    private String mName;
+    private String mPhone;
+    private String mLic;
+    private String mVec;
+    private String mTyp;
+    private String mProfileImageUrl;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_settings);
 
+        mNameField = (EditText) findViewById(R.id.name);
+        mPhoneField = (EditText) findViewById(R.id.phone);
+        mLicense = (EditText) findViewById(R.id.license);
+        mVehicle = (EditText) findViewById(R.id.vehicle);
+        mType = (EditText) findViewById(R.id.type);
 
-        Spinner dropdown = findViewById(R.id.Type);
-        String[] items = new String[]{"Bus", "Car", "MotorBike"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, items);
-        dropdown.setAdapter(adapter);
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
+
+        mBack = (Button) findViewById(R.id.confirm);
+        mConfirm = (Button) findViewById(R.id.back);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+        mDriverDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(userID);
+
+        getUserInfo();
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUserInformation();
+            }
+        });
+
+
+
     }
 
+    private void saveUserInformation() {
+        mName = mNameField.getText().toString();
+        mPhone = mPhoneField.getText().toString();
+        mLic = mLicense.getText().toString();
+        mVec = mVehicle.getText().toString();
+        mTyp = mType.getText().toString();
+
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("name", mName);
+        userInfo.put("phone", mPhone);
+        userInfo.put("license number", mLic);
+        userInfo.put("vehicle number", mVec);
+        userInfo.put("vehicle type", mTyp);
+        mCustomerDatabase.updateChildren(userInfo);
+        if(resultUri !=null){
+            final StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap = null;
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Map newImage = new HashMap();
+                            newImage.put("profileImageUrl", uri.toString());
+                            mCustomerDatabase.updateChildren(newImage);
+
+                            finish();
+                            return;
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            finish();
+                            return;
+                        }
+                    });
+                }
+            });
+
+        }else {
+            finish();
+        }
+    }
+
+    private void getUserInfo() {
+        mCustomerDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    assert map != null;
+                    if (map.get("name") != null) {
+                        mName = Objects.requireNonNull(map.get("name")).toString();
+                        mNameField.setText(mName);
+                    }
+                    if (map.get("phone") != null) {
+                        mPhone = Objects.requireNonNull(map.get("phone")).toString();
+                        mPhoneField.setText(mPhone);
+                    }
+                    if (map.get("license number") != null) {
+                        mLic = Objects.requireNonNull(map.get("license number")).toString();
+                        mLicense.setText(mLic);
+                    }
+                    if (map.get("vehicle number") != null) {
+                        mVec = Objects.requireNonNull(map.get("vehicle number")).toString();
+                        mVehicle.setText(mVec);
+                    }
+                    if (map.get("vehicle type") != null) {
+                        mTyp = Objects.requireNonNull(map.get("vehicle type")).toString();
+                        mType.setText(mTyp);
+                    }
+                    if (map.get("profileImageUrl") != null) {
+                        mProfileImageUrl = Objects.requireNonNull(map.get("profileImageUrl")).toString();
+                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
+    }
 }
